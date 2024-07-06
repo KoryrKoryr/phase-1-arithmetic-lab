ad74c1f552d61407c70eaaa24c4bbdf407e642ba
Denis Korir <denohrkoryr@gmail.com>
2024-07-06 14:23:45 +0300

    Arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+// Multiply
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+// Random
+
+const random = Math.ceil(Math.random() * 10) + 1;
+
+console.log(random);
+
+// Mod
+
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+// Max
+
+const number = (3, 6, 14, 8, 10, 9, 5, 16, 2, 0, 11, 20);
+
+const max = Math.max(number);
+
+console.log(max);
